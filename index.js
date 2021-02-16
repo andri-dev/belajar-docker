fc5a859f6a96736fd5b777fe23ee08bd943d076c
andrijunaedi <andrijunaedi126@gmail.com>
2021-02-16 07:35:17 +0700

    add Environment Variable

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 express()
-  .get('/', (req, res) => res.send('Hello World!'))
+  .get('/', (req, res) =>
+    res
+      .status(200)
+      .send({ data: `Hello World! \nMy Name ${process.env.NAME || 'Ninja'}` }),
+  )
   .listen(8080, () => console.log('Sever running on PORT 8080'));
