e267179c3204c1f0a141ac467784e86cbf7e13f2
andrijunaedi <andrijunaedi126@gmail.com>
2021-02-23 05:16:26 +0700

    Integration container with network

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-express()
-  .get('/', (req, res) =>
-    res
-      .status(200)
-      .send({ data: `Hello World! \nMy Name ${process.env.NAME || 'Ninja'}` }),
-  )
-  .listen(8080, () => console.log('Sever running on PORT 8080'));
+const listen = () => {
+  express()
+    .get('/', (req, res) =>
+      res.status(200).send({
+        data: `Hello World!, My Name ${process.env.NAME || 'Ninja'}`,
+      }),
+    )
+    .listen(8080, () => console.log('Server running on PORT 8080'));
+};
+
+const connect = () => {
+  mongoose.connection
+    .on('error', console.log)
+    .on('disconnected', connect)
+    .once('open', listen);
+  return mongoose.connect(
+    `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/belajar-docker`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  );
+};
+
+connect();
